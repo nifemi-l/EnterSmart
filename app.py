1b2b2a608df9ded87ae48a8e232f3c95899aea8d
Nifemi Lawal <lawalwaryth@gmail.com>
2024-11-24 15:11:47 -0600

    remove print statement, space code out

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def submit():
         }])
     
     df = pd.concat([df, new_row], ignore_index=True)
+
     df.to_excel(file_path, index=False)
     
     return jsonify({'message': 'Excel file updated successfully'})
@@ -31,12 +32,18 @@ def submit():
 @app.route('/view-history', methods=['GET'])
 def view_application_history(): 
     df = pd.read_excel('data.xlsx', usecols=['Company', 'Position', 'Location', 'Assessment Extended?', 'Interview Extended?', 'Offered?'])
+
     df['Assessment Extended?'].fillna('N/A', inplace=True)
+
     df['Interview Extended?'].fillna('N/A', inplace=True)
+    
     df['Offered?'].fillna('N/A', inplace=True)
-    print(df)
+
+    # print(df)
     data = df.to_dict(orient='records')
+
     data.reverse()
+
     return jsonify(data)
 
 @app.route('/application-history.html')
